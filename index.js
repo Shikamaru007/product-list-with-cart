73ce2e8eaf275cabd5c4dd826629fbe7a82cea13
froZin <bolutolu12@gmail.com>
2024-10-04 13:52:57 +0100

    on it

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const productGrid = document.getElementById("productGrid");
+const cartList = document.getElementById("cartList");
+const emptyCart = document.getElementById("emptyCart");
+let cart = [];
 
 fetch(`./data.json`).then(response => response.json()).then(data => {
     displayProduct(data);
@@ -10,7 +13,7 @@ function displayProduct(products){
         productCard.classList.add("product-card");
         productCard.innerHTML = `<div class="image">
         <img src="${product.image.desktop}" alt="${product.name}">
-        <button class="btn" id="btn" onclick="addToCart(${product.name}, ${product.price})"><img src="assets/images/icon-add-to-cart.svg" alt="">Add to Cart
+        <button class="btn" onclick="addToCart(${product.name}, ${product.price})" id="btn"><img src="assets/images/icon-add-to-cart.svg" alt="">Add to Cart
         <div class="productCount">
             <img src="assets/images/icon-decrement-quantity.svg">
             <span class="quantityCount">0</span>
@@ -23,7 +26,10 @@ function displayProduct(products){
         <p>$${product.price.toFixed(2)}</p>`;
 
         productGrid.appendChild(productCard);
-    })
+
+
+    })        
 
 
 }
+
